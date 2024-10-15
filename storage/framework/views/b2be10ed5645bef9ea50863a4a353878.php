90c9f98605de21971ea3f7c26e431245187f12c7
deshika99 <hansini199971@gmail.com>
2024-10-15 22:49:24 +0530

    update

--- storage/framework/views/b2be10ed5645bef9ea50863a4a353878.php
+++ storage/framework/views/b2be10ed5645bef9ea50863a4a353878.php
@@ -1,3 +1,4 @@
+
 <style>
     .navbar-divider {
     height: 40px;
@@ -20,6 +21,20 @@
     vertical-align: middle; 
 }
 
+.search-item {
+        padding: 10px;
+        border-bottom: 1px solid #ccc;
+    }
+
+    .search-item a {
+        text-decoration: none;
+        color: #000;
+    }
+
+    .search-item:hover {
+        background-color: #f1f1f1;
+    }
+
 </style>
         <header>
             <div class="text-center bg-white border-bottom">
@@ -35,12 +50,14 @@
                     </div>
 
 
-                        <div class="col-md-5 mt-2">
-                            <form class="d-flex input-group w-auto my-auto mb-3 mb-md-0">
-                                <input autocomplete="off" type="search" class="form-control rounded" placeholder="Search" />
-                                <span class="input-group-text border-0 d-none d-lg-flex"><i class="fas fa-search"></i></span>
-                            </form>
-                        </div>
+                    <div class="col-md-5 mt-2">
+                        <form class="d-flex input-group w-auto my-auto mb-md-0">
+                           <input autocomplete="off" id="search" type="search" class="form-control rounded" placeholder="Search" style="width: 250px;" />
+                           <span class="input-group-text border-0 d-none d-lg-flex"><i class="fas fa-search"></i></span>
+                        </form>
+                            <div id="search-results" style="display:none; position:absolute; background:white; width:38%; border:1px solid #ccc; z-index: 1000;"></div>
+                    </div>
+
 
                         <div class="col-md-3 mb-2 d-flex justify-content-center justify-content-md-end align-items-center">
                             <div class="d-flex align-items-center">
@@ -252,7 +269,6 @@ unset($__errorArgs, $__bag); ?>
                         <a href="<?php echo e(route('special_offerproducts')); ?>" class="mx-3">Special Offers</a>
                         <a href="<?php echo e(route('sale_products')); ?>" class="mx-3">Flash Sale</a>
                         <a href="<?php echo e(route('best_sellers')); ?>" class="mx-3">Bestsellers</a>
-                        <a href="#" class="mx-3">Super Deals</a>
                     </div>
                 </div>
         </div>
@@ -282,9 +298,53 @@ unset($__errorArgs, $__bag); ?>
             });
         });
 
+    </script>    
+    
+  
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+     <script type="text/javascript">
+       $(document).ready(function() {
+        $('#search').on('keyup', function() {
+            var query = $(this).val();
+            if (query.length > 2) {
+                $.ajax({
+                    url: "<?php echo e(route('searchProducts')); ?>", // Route to handle search
+                    type: "GET",
+                    data: { search: query },
+                    success: function(data) {
+                        $('#search-results').empty().show(); // Clear previous results and show dropdown
+                        if (data.length > 0) {
+                            $.each(data, function(index, product) {
+                                $('#search-results').append(
+                                    '<div class="search-item"><a href="/product/' + product.product_id + '">' + product.product_name + '</a></div>'
+                                );
+                            });
+                        } else {
+                            $('#search-results').append('<div class="search-item">No products found</div>');
+                        }
+                    },
+                    error: function() {
+                        $('#search-results').append('<div class="search-item">Error searching products</div>');
+                    }
+                });
+            } else {
+                $('#search-results').hide(); // Hide dropdown if query is too short
+            }
+        });
 
-        
-    </script>
+        // Hide results when clicking outside of the search input
+        $(document).click(function(e) {
+            if (!$(e.target).closest('#search, #search-results').length) {
+                $('#search-results').hide();
+            }
+        });
+    });
+    
+</script>
+
+
+   
+    
 
 
 
